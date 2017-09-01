cc316e1e0e3050f87638fdbfb01b16d9d514cf00
ser <ser@gg>
2017-09-01 16:28:36 +0300

    xz

--- krug/1/graf.js
+++ krug/1/graf.js
@@ -5,140 +5,99 @@ height = +svg.attr("height") ;
 	
 var color = d3.scaleOrdinal(prop.colorsLeft);
 
-
-function countInnerDataForPie(d){
-	
-	for (k in d){
-		var sum = 0;
-		for ( key in d[k].value) {
-			for (dat in d[k].value[key] ) {
-				sum+= +(d[k].value[key][dat]) ; 
-			}
-			d[k].sum = sum ; 
-		}
-	}
-	return d
-}
 radius = 100 ;
 function showPieLeft(data){
 	
-	var pie = d3.pie().sort(null).value(function(d) {
-		return d['sum'];
+	g = svg.append('g')
+			.attr("transform", "translate(" + width *0.2  + "," + height / 2 + ")") 
+			.attr("class" , "donut"); 
+	donut = g.append("g")
+			.attr("class", "donut");
+
+	var pie = d3.pie().sort(null).value(function(d , i ) {
+		return d;
 	});
 	var path = d3.arc()
 					.outerRadius(radius)
 					.innerRadius(40);
-	var label = d3.arc()
-					.outerRadius( radius )
-					.innerRadius(radius / 3.5);
-	
-	var arc = innerDonut.selectAll(".arc")
-								.data(pie(data)).enter().append("g")
+	var arc = donut.selectAll(".arc")
+								.data( pie([  data[0] , 0  ]) ).enter().append("g")
 														.attr("class", "arc");
 
+	console.log(  (pie(data))[0]  ); 
+	
+	
 	arc.append("path")
 			.attr("d", path)
-			.attr("fill", function(d , i ) { 
-				data2[i].startAngle = d.startAngle ;  
-				data2[i].endAngle = d.endAngle ;  
-				return color(d.data['name']); });
-
-	arc.append("text")
-		  .attr("transform", function(d) {  return "translate(" + label.centroid(d) + ")"; })
+			.attr("fill", function(d , i ) {
+				return color(d.data); });
+	
+	donut.select(".arc").
+	append("text")
+		  .attr("transform", function(d) {  return "translate(5, 0)"; })
 		  .attr("text-anchor"  , "middle" )
 		  .attr("dy", "0.35em")
 		  .attr("font-family" , "sans-serif")
-		  .attr("font-size" , "14")
-		  .attr("fill" , "white")
-		  .attr("font-weight" , "bold")
-		  .text(function(d) {  return ( (d.endAngle - d.startAngle)/6.28 * 100).toFixed(1)   + '%'; });
-}
-
-
-function showPiePercents(data , index){
-	var radiusPlus = 40 ;
-	var red = d3.scaleLinear()
-					.domain([0 , (data[index].sum / 6) ]  )
-					.range(["white" , prop.colorsLeft[index]  ]);	
-	var pie = d3.pie()
-				.sortValues(function compare(a, b) {
-					  return b - a;
-				})
-				.value(function(d) { return d3.values(d);  })
-				.startAngle(data[index].startAngle)
-				.endAngle(data[index].endAngle) ;
+		  .attr("font-size" , "20")
+//		  .attr("font-weight" , "bold")
+		  .text(function(d) {  return  ( (   data [1] / (data[1]+data[0]) ) * 100).toFixed(1)   + '%'; });
 	
-	var path = d3.arc()
-					.outerRadius(radius + radiusPlus)
-					.innerRadius(radius + 5 ) ;
-	var label = d3.arc()
-					.outerRadius( radius )
-					.innerRadius(radius +radiusPlus + 90);
-	var arc = donut.selectAll(".arc")
-						.data(pie(data[index].value)).enter().append("g")
-							.attr("class", "arc");
-
-	arc.append("path")
-			.attr("d", path)
-			.attr("fill", function(d) {  return red(d3.values(d.data)); })
-			;
-
-	arc.append("text")
-		  .attr("transform", function(d) {  return "translate(" + label.centroid(d) + ")"; })
+	
+	donut.select(".arc").
+	append("text")
+		  .attr("transform", function(d) {  return "translate( 0 , "+ (radius+20) + ")"; })
 		  .attr("text-anchor"  , "middle" )
 		  .attr("dy", "0.35em")
 		  .attr("font-family" , "sans-serif")
-		  .attr("font-size" , "14")
-		  .attr("fill" , "black")
-		  .attr("font-weight" , "bold")
-		  .text(function(d) {  return ( (d.endAngle - d.startAngle)/6.28 * 100).toFixed(1)   + '%'; });
-}
-
-
-
-data2 = countInnerDataForPie(data2);
-g = svg.append('g')
-		.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")") 
-		.attr("class" , "donut"); 
-donut = g.append("g")
-			.attr("class", "donut");
-innerDonut = g.append("g")
-				.attr("class", "innerDonut");
-
-
-showPieLeft(data2 );
-accr() ; 
-//showPiePercents(data2  ,0);
-
-function accr(){
-	document.write('<div class="accord"> ');
-	for (i =0 ; i < data2.length ;i ++) {
-		document.write('<button class="accordion" count='+i+'>' +data2[i].name + '</button> ');
-		document.write('<div class="panel">');
-		for (dat in data2[i].value) {
-			document.write('  <p> ' +  Object.keys(data2[i].value[dat])  + ' </p>');
-		}
-		document.write('</div>');
-	}
-	document.write('</div>');
+		  .attr("font-size" , "20")
+		  .text("Доходы");
 	
-	var acc = document.getElementsByClassName("accordion");
-	var i;
+	textg = donut.select(".arc").append("g") 
+									.attr("class", "textCaprion");
+	textg.append("text")
+		  .attr("transform", function(d) {  return "translate( 0 , "+ (radius+50) + ")"; })
+		  .attr("text-anchor"  , "middle" )
+		  .attr("dy", "0.35em")
+		  .attr("font-family" , "sans-serif")
+		  .attr("font-size" , "20")
+		  .text("План"  );
+	
+	
+	textg.append("text")
+	  .attr("transform", function(d) {  return "translate( 0 , "+ (radius+70) + ")"; })
+	  .attr("text-anchor"  , "middle" )
+	  .attr("dy", "0.35em")
+	  .attr("font-family" , "sans-serif")
+	  .attr("font-size" , "20")
+	  .text(currencySwap ( data[0])  );
 
-	for (i = 0; i < acc.length; i++) {
-		
-	  acc[i].onclick = function() {
-	    this.classList.toggle("active");
-	    showPiePercents(data2 , this.getAttribute("count"));
-	    var panel = this.nextElementSibling;
-	    if (panel.style.maxHeight){
-	      panel.style.maxHeight = null;
-	    } else {
-	      panel.style.maxHeight = panel.scrollHeight + "px";
-	    } 
-	  }
-	}
+	textg.append("text")
+	  .attr("transform", function(d) {  return "translate( 0 , "+ (radius+90) + ")"; })
+	  .attr("text-anchor"  , "middle" )
+	  .attr("dy", "0.35em")
+	  .attr("font-family" , "sans-serif")
+	  .attr("font-size" , "20")
+	  .text("Факт"  );
+
+	textg.append("text")
+	  .attr("transform", function(d) {  return "translate( 0 , "+ (radius+110) + ")"; })
+	  .attr("text-anchor"  , "middle" )
+	  .attr("dy", "0.35em")
+	  .attr("font-family" , "sans-serif")
+	  .attr("font-size" , "20")
+	  .text(currencySwap ( data[1])  );
+
+	
 }
+
+function currencySwap(d){
+    return d.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")  + " руб" ;
+}
+
+
+
+
+showPieLeft(data);
 
 
 
