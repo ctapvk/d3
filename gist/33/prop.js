d04aebe37660bc144b0292955a29c13fa102c73d
ctapvk <ctapvk@mail.ru>
2017-09-20 17:29:09 +0300

    gist/55

--- gist/33/prop.js
+++ gist/33/prop.js
@@ -1,21 +1,21 @@
 prop = {
 
-"доходы план" :"#fad9a9" , 
-"доходы факт" :"#f5ba63" , 
-"расходы план": "#94c0de" , 
-"расходы факт" : "#529acb"  , 
+"доходы план" :"#fad9a9" ,
+"доходы факт" :"#f5ba63" ,
+"расходы план": "#94c0de" ,
+"расходы факт" : "#529acb"  ,
 "деф проф план" : "#f3b3b2" ,
-"деф проф факт" : "#f08482" , 
+"деф проф факт" : "#f08482" ,
 
-    "spaceBetween" : "20" , 
-    "paddingLeft" : "120" , 
-    "paddingBottom" : "150"  , 
-    "moveMounth":"20" , 
+    "spaceBetween" : "20" ,
+    "paddingLeft" : "120" ,
+    "paddingBottom" : "150"  ,
+    "moveMounth":"20" ,
     "gistPadding": "30"
 }
 
 
-data3 = 
+data3 =
 [
 {"name":"01.07.2013", "dohodPlan":930000,"dohodFact":540000, "rashodPlan":790000, "rashodFact":600000, "deficitPlan":100000, "deficitFact":90000},
 {"name":"01.07.2014", "dohodPlan":490000,"dohodFact":45000, "rashodPlan":660000, "rashodFact":520000, "deficitPlan":100000, "deficitFact":10000},
