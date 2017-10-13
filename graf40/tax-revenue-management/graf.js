b79ac6e2efa685ebd057e1ba66e61bd40532b1b4
ser <ser@gg>
2017-10-13 14:32:32 +0300

    http://cp.volgafin.rosoperator.ru/26-tax-revenue-management.html done

--- graf40/tax-revenue-management/graf.js
+++ graf40/tax-revenue-management/graf.js
@@ -1,46 +1,3 @@
-function drawGraph20() {
-// init block
-    var svg = d3.select(".graf20");
-
-    widthSvg = +svg.attr("width");
-    heightSvg = +svg.attr("height");
-    width = +svg.attr("width") ;
-    height = +svg.attr("height");
-
-    center = svg.append("g")
-        .attr("class", "center")
-        .attr("transform", "translate("+[ width/2,  height/2  ]+")")
-    ;
-
-    x = d3.scaleLinear()
-        .domain([0  , 100 ] )
-        .range([ 0 ,  +prop20.radius   ])
-    ;
-
-// init block
-
-
-
-    function drawBase(canvas){
-        te = canvas.append("g").attr("class" , "basePie");
-
-        pie = d3.pie().padAngle(.0);
-        arc = d3.arc();
-
-        pie( [1,2]).forEach( function (t, i ) {
-            t.outerRadius = 100 ;  t.innerRadius = 30;
-            te.append("path")
-                .attr("fill" ,  prop20.colorsLeft[i] )
-                .attr("d", arc(t))
-            ;
-        });
-
-    }
-
-
-    drawBase(center);
-}
-
 
 
 
@@ -49,6 +6,7 @@ function drawGraph20() {
 function gist8() {
 
     svg = d3.select(".gist8");
+    svg.selectAll("*").remove();
     data8 = dataProcc(data8);
 
     widthSvg = +svg.attr("width");
@@ -83,27 +41,31 @@ function gist8() {
     ;
 
 // console.log([max ,  min]);
-    if (data8.length < 1) {
+    if (data8.length < 7) {
         x = d3.scaleLinear()
-            .domain([0, data8.length])
-            .range([+prop8.gistPadding, ( +prop8.barSize + +prop8.spaceBetween) * data8.length])
+            .domain([0, 6])
+            .range([+prop8.gistPadding, ( +prop8.barSize + +prop8.spaceBetween) * 6])
         ;
+        rotText = "";
         barSize = +prop8.barSize;
     } else {
         x = d3.scaleLinear()
-            .domain([0, data8.length])
-            .range([+prop8.gistPadding, width])
+            .domain([0, data8.length-1])
+            .range([ 5 , (+prop8.barSize-5 )*11])
         ;
-        barSize = width / data8.length - +prop8.spaceBetween;
+        rotText = "rotate(30 )";
+        barSize = +prop8.barSize-5; ;
     }
 
     drawBack(asisX);
     drawAsisY(asisY);
     showLegend(asisX);
-    showPlanIn(gist);
+    drawNach(gist);
+    drawpostup(gist);
 
 
     drawAsisX(gist);
+
 
     function drawAsisX(canvas) {
 
@@ -172,7 +134,7 @@ function gist8() {
         legend.append("text")
             .attr("class", "asisYcapiton")
             .attr("transform", "translate( " + [-10, 10] + ")")
-            .text("млн. руб.")
+            .text("тыс. руб.")
         ;
     }
 
@@ -201,28 +163,75 @@ function gist8() {
     }
 
 
+    div = d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
 
-
-    function showPlanIn(canvas) {
+    function drawNach(canvas) {
         rects = canvas.append("g");
 
         data8.forEach(function (d, i) {
 
-            rectHeight = y(d.zadolzhen);
+            rectHeight = y(d.nach);
+
             rects.append("rect")
                 .attr("transform", function () {
-                    return "translate( " + [x(i), -height + y(d.zadolzhen)] + ")";
+                    return "translate( " + [x(i), -height + y(d.nach)] + ")";
                 })
                 .attr("width", barSize)
                 .attr("height", function () {
                     return -(rectHeight - y(0))
                 })
-                .attr("fill", prop8.colorPlanIn)
+                .attr("fill", prop8.colornach)
+                .on("mousemove", function() {
+                    div.transition().duration(200).style("opacity", .9);
+                    div.html(    currencySwapNoCut(d.nach) )
+                        .style("left", (d3.event.pageX) + "px")
+                        .style("top", (d3.event.pageY - 30 ) + "px")
+                    ;
+                })
+                .on("mouseout", function() { div.transition().duration(500).style("opacity", 0); })
+                .on("click", function() { drawGraph20(d.dat);  })
+
+            ;
+            rects.append("text")
+                .attr("class", "planGistLabel")
+                .attr("transform", "translate( " + [x(i) + barSize / 2, -height + rectHeight - 15] + ")")
+                .text(cutLongSum(d.nach))
+
+            ;
+        });
+    }
+
+    function drawpostup(canvas) {
+        rects = canvas.append("g");
+
+        data8.forEach(function (d, i) {
+
+            rectHeight = y(d.postup);
+            rects.append("rect")
+                .attr("transform", function () {
+                    return "translate( " + [x(i), -height + y(d.postup)] + ")";
+                })
+                .attr("width", barSize)
+                .attr("height", function () {
+                    return -(rectHeight - y(0))
+                })
+                .attr("fill", prop8.colorpostup)
+                .on("mousemove", function() {
+                    div.transition().duration(200).style("opacity", .9);
+                    div.html(   currencySwapNoCut(d.postup) )
+                        .style("left", (d3.event.pageX) + "px")
+                        .style("top", (d3.event.pageY - 30 ) + "px")
+                    ;
+                })
+                .on("mouseout", function() { div.transition().duration(500).style("opacity", 0); })
+                .on("click", function() { drawGraph20(d.dat);  })
+
+
             ;
             rects.append("text")
                 .attr("class", "factGistLabel")
                 .attr("transform", "translate( " + [x(i) + barSize / 2, -height + rectHeight + 15] + ")")
-                .text(cutLongSum(d.zadolzhen))
+                .text(cutLongSum(d.postup))
             ;
         });
     }
@@ -240,9 +249,10 @@ function gist8() {
 
         datas = canvas.append("g").attr("transform", "translate(0,0)");
 
+
         data8.forEach(function (d, i) {
             datas.append("text")
-                .attr("transform", "translate(" + [x(i) + barSize / 2, +prop8.moveMounth] + ")")
+                .attr("transform", " translate(" + [x(i) + barSize / 2, +prop8.moveMounth] + ") "+ rotText+" ")
                 .attr("class", "mounthLabes")
                 .text(wrapDate(d.date))
             ;
@@ -251,43 +261,30 @@ function gist8() {
         legend = canvas.append("g").attr("transform", "translate(60,0)");
 
 
-        outLeg = legend.append("g").attr("transform", "translate(200,60)");
+        outLeg = legend.append("g").attr("transform", "translate(0,60)");
         outLeg.append("rect")
             .attr("width", 20)
             .attr("height", 20)
-            .attr("fill", prop8.colorPlanIn)
+            .attr("fill", prop8.colornach)
         ;
         outLeg.append("text")
             .attr("transform", "translate(30 , 15)")
             .attr("class", "legend")
-            .text("Задолженность")
+            .text("Начислено к уплате")
         ;
 
-        saldo = legend.append("g").attr("transform", "translate(350,60)");
-
-        saldo.append("text")
-            .attr("transform", "translate(120 , 15)")
+        outLeg = legend.append("g").attr("transform", "translate(200,60)");
+        outLeg.append("rect")
+            .attr("width", 20)
+            .attr("height", 20)
+            .attr("fill", prop8.colorpostup)
+        ;
+        outLeg.append("text")
+            .attr("transform", "translate(30 , 15)")
             .attr("class", "legend")
-            .text("Сальдо")
-        ;
-        line = d3.line().x(function (d) {
-            return d[0]
-        }).y(function (d) {
-            return d[1]
-        });
-        saldo.append("path")
-            .attr("d", line([[20, 10], [80, 10]]))
-            .style("stroke", prop8.colorSaldo)
-            .style("stroke-width", 4)
+            .text("Поступления в бюджеты")
         ;
 
-        saldo.append("circle")
-            .style("fill", prop8.colorSaldo)
-            .attr("class", "dot")
-            .attr("r", 6)
-            .attr("cx", 50)
-            .attr("cy", 10)
-        ;
 
 
     }
@@ -297,15 +294,19 @@ function gist8() {
     function findMaxVal(d) {
         max = 0;
         for (i in d)
-            if (parseFloat(max) < parseFloat(d[i].zadolzhen)) max = d[i].zadolzhen;
+            if (parseFloat(max) < parseFloat(d[i].nach)) max = d[i].nach;
+        for (i in d)
+            if (parseFloat(max) < parseFloat(d[i].postup)) max = d[i].postup;
         return max;
 
     }
 
     function dataProcc(d) {
-        for (i in d) {
-            if (d[i].zadolzhen < 0) d[i].zadolzhen = 0;
-        }
+        for (i in d)
+            if (d[i].nach < 0) d[i].nach = 0;
+        for (i in d)
+            if (d[i].postup < 0) d[i].postup = 0;
+
 
         return d;
 
@@ -324,6 +325,26 @@ function gist8() {
         return d.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") + "";
     }
 
+
+    function cutLongText (str) {
+        str = str[0];
+        if (str.length > 25)
+            return str.substr(0,25) + "..." ;
+        else
+            return str ;
+    }
+
+
+    function currencySwapWithText(d) {
+        d= parseInt(parseFloat(d)*0.001 );
+        return d.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " тыс. руб.";
+    }
+
+    function currencySwapNoCut(d) {
+        d= parseInt(parseFloat(d) );
+        return d.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " руб.";
+    }
+
     function cutLongSum(d) {
         if (+d > 1000)
             return d.toString().substr(0, 3);
@@ -332,6 +353,99 @@ function gist8() {
     }
 
 
+
+    function drawGraph20(data) {
+// init block
+        var svg = d3.select(".graf20");
+        svg.selectAll("*").remove();
+
+        widthSvg = +svg.attr("width");
+        heightSvg = +svg.attr("height");
+        width = +svg.attr("width") ;
+        height = +svg.attr("height");
+
+        center = svg.append("g")
+            .attr("class", "center")
+            .attr("transform", "translate("+[ width/2, +prop8.radius+30  ]+")")
+        ;
+
+        x = d3.scaleLinear()
+            .domain([0  , 100 ] )
+            .range([ 0 ,  +prop8.radius   ])
+        ;
+
+// init block
+
+
+
+        function drawBase(canvas){
+            te = canvas.append("g").attr("class" , "basePie");
+
+            pie = d3.pie().padAngle(.0).sort(null);
+            arc = d3.arc();
+
+            dat = [];
+            data.forEach(function (t) {
+                dat.push((Object.values(t))[0])
+            })
+
+
+            pie( dat ).forEach( function (t, i ) {
+                t.outerRadius = prop8.radius ;  t.innerRadius = 35;
+                te.append("path")
+                    .attr("fill" ,  prop8.colorsKrug[i] )
+                    .attr("d", arc(t))
+                    .on("mousemove", function(d) {
+                        div.transition()
+                            .duration(200)
+                            .style("opacity", .9);
+                        div.html(
+                            Object.keys(data[i]) +" : " + currencySwapNoCut(t.data ) )
+                            .style("left", (d3.event.pageX) + "px")
+                            .style("top", (d3.event.pageY - 28) + "px");
+                    })
+                    .on("mouseout", function(d) {
+                        div.transition()
+                            .duration(500)
+                            .style("opacity", 0);
+                    })
+                ;
+
+                if ( +(t.data / d3.sum(dat))*100 >11)
+                    txt=( (t.data / d3.sum(dat)).toFixed(3) * 100).toFixed(1) + '%' ;   else txt="";
+
+                te.append("text")
+                    .attr("class" , "krugPieTextLegend")
+                    .attr("transform" , "translate(" + d3.arc().centroid(t) + ")")
+                    .text( txt )
+                ;
+            });
+
+        }
+
+        function drawLegend(canvas) {
+            te = canvas.append("g")
+                .attr("transform" , "translate("+[ 50 ,  +prop8.radius*2 + 100 ]+")")
+            ;
+            caps = ["Поступления в ФБ","Поступления в бюджет Волгоградской области"];
+            data.forEach(function (d , i ) {
+                leg = te.append("g").attr("transform" , "translate("+[ 0, i*40  ]+")");
+
+                leg.append("text")
+                    .attr("class", "krugLegText").text( cutLongText(Object.keys(data[i]) ) )
+                ;
+                leg.append("rect")
+                    .attr("class" , "krugRectLeg")
+                    .attr("transform" , "translate("+[ -35,-18 ]+")")
+                    .attr("fill", prop8.colorsKrug[i])
+                ;
+
+            })
+        }
+
+        drawLegend(svg);
+        drawBase(center);
+    }
 
 
 
