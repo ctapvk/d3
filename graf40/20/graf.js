77b995d908f8a026cfb26e413e13c3d5df7a76d6
ser <ser@gg>
2017-10-16 12:39:48 +0300

    graf40/20/

--- graf40/20/graf.js
+++ graf40/20/graf.js
@@ -1,6 +1,8 @@
-function drawGraph20() {
+function drawGraph20(prop, data , index) {
 // init block
     var svg = d3.select(".graf20");
+    svg.selectAll("*").remove();
+    d3.selectAll(".tooltip").remove();
 
     widthSvg = +svg.attr("width");
     heightSvg = +svg.attr("height");
@@ -9,13 +11,15 @@ function drawGraph20() {
 
     center = svg.append("g")
         .attr("class", "center")
-        .attr("transform", "translate("+[ width/2,  height/2  ]+")")
+        .attr("transform", "translate("+[ width/2,  +prop.radius + 10  ]+")")
     ;
 
     x = d3.scaleLinear()
         .domain([0  , 100 ] )
-        .range([ 0 ,  +prop20.radius   ])
+        .range([ +prop.radiusOfHole  ,  +prop.radius   ])
     ;
+
+    div = d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
 
 // init block
 
@@ -24,19 +28,82 @@ function drawGraph20() {
     function drawBase(canvas){
         te = canvas.append("g").attr("class" , "basePie");
 
-        pie = d3.pie().padAngle(.0);
+        pie = d3.pie().padAngle(.0).sort(null);
         arc = d3.arc();
 
-        pie(data20.base.value).forEach( function (t, i ) {
-            t.outerRadius = 200 ;  t.innerRadius = 30;
+        pie(data.base.value).forEach( function (t, i ) {
+            t.outerRadius = +prop.radius;  t.innerRadius = +prop.radiusOfHole ;
             te.append("path")
-                .attr("fill" ,  prop20.colorsLeft[i] )
+                .attr("fill" ,  prop.colorsBase[i] )
                 .attr("d", arc(t))
+                .on("mousemove", function(d) {
+                    div.transition().duration(200).style("opacity", .9);
+                    div.html( data.base.value[i] )
+                        .style("left", (d3.event.pageX) + "px")
+                        .style("top", (d3.event.pageY - 28) + "px");
+                })
+                .on("mouseout", function(d) {
+                    div.transition().duration(500).style("opacity", 0);
+                })
+
             ;
+        });
+
+    }
+
+    function drawOverBase(canvas , index){
+        te = canvas.append("g").attr("class" , "OverPie");
+
+        pie = d3.pie().padAngle(.0).sort(null);
+        arc = d3.arc();
+        label = d3.arc();
+
+
+        pie(data.base.value).forEach( function (t, i ) {
+            var per =   (data.vals[index].value[i]/ data.base.value[i]).toFixed(2)*100 ;
+            t.outerRadius = x(per) ;  t.innerRadius = +prop.radiusOfHole;
+            te.append("path")
+                .attr("fill" ,  prop.colorsOverBase[i] )
+                .attr("d", arc(t))
+                .on("mousemove", function(d) {
+                    div.transition().duration(200).style("opacity", .9);
+                    div.html( data.vals[index].name+ ' : ' + data.vals[index].value[i]  +' ( '+ per + '% )' )
+                        .style("left", (d3.event.pageX) + "px")
+                        .style("top", (d3.event.pageY - 28) + "px");
+                })
+                .on("mouseout", function(d) {
+                    div.transition().duration(500).style("opacity", 0);
+                })
+
+            ;
+            t.outerRadius = +prop.radiusOfHole+60 ;  t.innerRadius = +prop.radiusOfHole;
+            if (per > 50)
+            te.append("text")
+                .attr("class" , "krugPieTextLegend")
+                .attr("transform", "translate("+  label.centroid(t)+")")
+                .text(per + '%')
+            ;
+
         });
 
     }
 
 
 drawBase(center);
+drawOverBase(center , index);
 }
+
+
+te = d3.select("#graf20Legend");
+s ='<div class="control-group">' ;
+data20.vals.forEach(function(d, i) {
+    // console.log(d);
+    s+= '<label class="control control--radio" >' + d.name ;
+    s+= '<input onclick="drawGraph20(prop20 ,data20 ,'+i +')" type="radio" name="radio"/>';
+    s+= '<div class="control__indicator"></div>';
+    s+= '</label>';
+
+})
+s+= "</div>";
+
+te.html(s);
