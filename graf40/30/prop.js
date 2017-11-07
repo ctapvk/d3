b8b12aef2c317e6d3512c64c66d93b8bcd3a9e11
ctapvk <ctapvk@mail.ru>
2017-11-07 15:09:00 +0300

    done 30 31

---
+++ graf40/30/prop.js
@@ -0,0 +1,16 @@
+propGraph30 = {
+"colorDoh" : "#4783AF" ,
+"colorRash" : "#DFBBA4"  ,
+"colorDef" : "#CF748D" ,
+}
+
+var dataGraph30 = [
+{"name":"Волгордаская область", "dohodPlan":3000000 , "rashodPlan":1000000,  "deficitPlan":-123 },  
+{"name":"Волгордаская область", "dohodPlan":-1000000 , "rashodPlan":1000000,  "deficitPlan":123 },  
+{"name":"Волгордаская область", "dohodPlan":2000000 , "rashodPlan":1000000,  "deficitPlan":-123123 },  
+{"name":"Волгордаская область", "dohodPlan":2000000, "rashodPlan":1000000,  "deficitPlan":-1123123 },  
+{"name":"Волгордаская область", "dohodPlan":2000000 , "rashodPlan":1000000,  "deficitPlan":2000000 },  
+
+]
+
+
---
+++ graf40/31/prop.js
@@ -0,0 +1,23 @@
+propGraph31 = {
+"colorDoh" : "#4783AF" ,
+"colorRash" : "#DFBBA4"  ,
+"colorDef" : "#CF748D" ,
+}
+
+var dataGraph31 = [
+{name:"01.06.2014" ,  
+vol: {"name":"Волгордаская область", "dohodPlan":3000000 , "rashodPlan":1000000,  "deficitPlan":-1200003 } ,
+obl: {"name":"Ростовская область", "dohodPlan":3000000 , "rashodPlan":1000000,  "deficitPlan":-1200003 } , 
+},  
+{name:"01.06.2014" ,  
+vol: {"name":"Волгордаская область", "dohodPlan":3000000 , "rashodPlan":1000000,  "deficitPlan":-1200003 } ,
+obl: {"name":"Ростовская область", "dohodPlan":3000000 , "rashodPlan":1000000,  "deficitPlan":-4200003 } , 
+},  
+{name:"01.06.2014" ,  
+vol: {"name":"Волгордаская область", "dohodPlan":3000000 , "rashodPlan":1000000,  "deficitPlan":-1200003 } ,
+obl: {"name":"Ростовская область", "dohodPlan":3000000 , "rashodPlan":1000000,  "deficitPlan":-0 } , 
+},  
+
+]
+
+
