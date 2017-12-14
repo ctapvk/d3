b26dd8ecd0f2ce53a08fe551746ec5a83219deef
ctapvk <ctapvk@mail.ru>
2017-12-14 21:59:23 +0300

    5

---
+++ graf40/5/prop.js
@@ -0,0 +1,15 @@
+propGraph36 = {
+    "faktColor" : "#529acb",
+    "planColor" : "#f5ba63" ,
+    "colors" : ["#f5ba63" ,"#529acb"] ,
+    "gistPadding": "30"
+}
+
+var dataGraph36 = [
+
+    {name:'second' , vals:[{one:6} , {two:1} , {two:1} , {two:2} , {two:1} , ] } ,
+    {name:'rst' , vals:[{one:5} , {two:1} , {two:1} , {two:1} , {two:1} ,{t22wo:1} , ] } ,
+
+]
+
+
