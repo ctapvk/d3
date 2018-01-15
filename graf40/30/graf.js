c25f914d2d1bd0499a379b110d997ce3b4b2db55
ser <ser@gg>
2018-01-15 14:31:36 +0300

    30 36

--- graf40/30/graf.js
+++ graf40/30/graf.js
@@ -5,14 +5,19 @@ function drawGraph30(data , prop , id) {
     prop.paddingBottom = 40 ;
     prop.paddingTop = 50 ;
     prop.paddingRight = 30 ;
+    prop.spaceBetween = 50 ;
+    prop.gistSize = 50 ;
     prop.backColor = "#acc" ;
-    prop.backColor = "#F8F9FA" ;
+    // prop.backColor = "#F8F9FA" ;
 
     // console.log(prop);
+    // console.log(JSON.stringify(data)) ;
+
+    gistSize = prop.gistSize  ;
 
     svg = d3.select("#"+id);
     svg.selectAll("*").remove();
-    
+
     div = d3.select("#divGraph30").append("div").attr("class", "tooltipGraph30").style("opacity", 0);
 
     widthSvg = +svg.attr("width");
@@ -55,31 +60,50 @@ function drawGraph30(data , prop , id) {
     // showBorders(asisY);
     // showBorders(asisXtop);
     // showBorders(asisYright);
-diff = 1.3 ; 
-min = findMaxVal2(data) ; 
-max = findMinVal2(data) ;
-y = d3.scaleLinear()
+    diff = 1.3 ;
+    min = findMaxVal2(data) ;
+    max = findMinVal2(data) ;
+
+
+    min = parseInt(min);
+    len = min.toString().length;
+    if (len <4) { de=1; deText = "руб."; }
+    if (len <7 && len>3) {de = 0.001; deText = "тыс. руб."; }
+    if (len <10 && len >6) { de=0.000001 ; deText = "млн. руб."; }
+    if (len <13 && len >9) { de=0.000000001 ; deText = "млрд. руб."; }
+    if (len <16 && len >12) { de=0.000000000001 ; deText = "трлн. руб."; }
+    if (len >15)  { de=0.000000000000001 ; deText = "квинт. руб."; }
+
+
+
+    y = d3.scaleLinear()
         .domain([max * diff, min * diff])
         .range([height, 0]);
-y2 = d3.scaleLinear()
+    y2 = d3.scaleLinear()
         .domain([max * diff, min * diff])
         .range([0, height]);
 
 // console.log([max ,  min]);
 // console.log( [ findMaxVal2(data) , findMinVal2(data) ] );
 
-
-x = d3.scaleLinear()
+    len =data.length ;
+    size = ( gistSize*2 + +prop.spaceBetween) * len ;
+    x = d3.scaleLinear()
         .domain([0  , data.length] )
-        .range([ 0 ,  width   ])
-;
-    barSize = +prop.barSize;
+        .range([ 0 ,  size  ])
+    ;
+    if (  size > width ) {
+        sizeSvg = +prop.paddingLeft + +prop.paddingRight + size  ;
+        svg.attr("width" , sizeSvg  );
+        width = sizeSvg ;
+        svg.attr('viewBox' , "0 0 "+ sizeSvg + " " +  svg.attr('height') ) ;
+    }
 
     function drawAsisX(canvas) {
 
         line = d3.line().x(function(d) {return d[0];} ).y(function(d) { return d[1]} ) ;
-        canvas.append("path") 
-            .attr("d", line([[0,  y(0)], [getElemWidth(canvas), y(0)]]))
+        canvas.append("path")
+            .attr("d", line([[0,  y(0)], [size, y(0)]]))
             .attr("stroke-width", 4)
             .attr("stroke", "#CDD5DE")
         ;
@@ -131,6 +155,7 @@ x = d3.scaleLinear()
                 ;
                 text.append("text")
                     .attr("text-anchor", "end")
+                    .attr("class", "asisYcapiton")
                     .attr("dominant-baseline", "central")
                     .attr("transform",  "translate(" + [-10, 0] + ")" )
                     .text( currencySwap(dat) )
@@ -144,191 +169,198 @@ x = d3.scaleLinear()
         });
 
         canvas.append("text")
-                .attr("transform", "translate(" + [getElemWidth(canvas)/2, 10] + ")")
-                .attr("text-anchor", "middle")
-                .text("тыс. руб.")
+            .attr("transform", "translate(" + [getElemWidth(canvas)/2, 10] + ")")
+            .attr("text-anchor", "middle")
+            .attr("class", "lebelgrag31")
+            .text(deText)
         ;
     }
 
 
 
-function showFactIn(canvas){
-    rects = canvas.append("g").attr("transform","translate("+[50,0]+")")
-    data.forEach(function(d , i){
-        bar = rects.append("g").attr("transform","translate("+[x(i),0]+")")
-                                .attr("width",x(1)-x(0))
-                                .attr("height",getElemHeight(canvas))
-        ;
+    function showFactIn(canvas){
+        rects = canvas.append("g").attr("transform","translate("+[50,0]+")")
+        data.forEach(function(d , i){
+            bar = rects.append("g").attr("transform","translate("+[x(i),0]+")")
+                .attr("width",x(1)-x(0))
+                .attr("height",getElemHeight(canvas))
+            ;
 
-        bar.append("text")
-            .attr("class","middleTextLegend")
-            .attr("transform","translate("+[gistSize, -30]+")")
-            .html(breakLongText( d.name, 6) )
-        ;
-        line = d3.line().x(function(d) {return d[0];} ).y(function(d) { return d[1]} ) ;
-        bar.append("path") 
-            .attr("d", line([[gistSize,  -10], [gistSize, 30]]))
-            .attr("stroke-width", 2)
-            .attr("stroke", "#999")
-        ;       
-        // showBorders(bar); 
-        showBar(bar, 0 ,d.dohodPlan , prop.colorDoh );
-        showBar(bar, 1 ,d.rashodPlan , prop.colorRash);
-        showBar(bar, 2 ,d.deficitPlan , prop.colorDef);
+            bar.append("text")
+                .attr("class","middleTextLegend30")
+                .attr("transform","translate("+[gistSize, -30]+")")
+                .html(breakLongText( d.name, 9) )
+            ;
+            line = d3.line().x(function(d) {return d[0];} ).y(function(d) { return d[1]} ) ;
+            bar.append("path")
+                .attr("d", line([[gistSize,  -10], [gistSize, 30]]))
+                .attr("stroke-width", 2)
+                .attr("stroke", "#999")
+            ;
+            // showBorders(bar);
+            showBar(bar, 0 ,d.dohodPlan , prop.colorDoh );
+            showBar(bar, 1 ,d.rashodPlan , prop.colorRash);
+            showBar(bar, 2 ,d.deficitPlan , prop.colorDef);
 
-    });
-}
+        });
+    }
 
-function showBar(canvas, index ,d ,color ){
-        rectHe =  y(0)-y(d)   ;  
+    function showBar(canvas, index ,d ,color ){
+        rectHe =  y(0)-y(d)   ;
         bar1 = canvas.append("g").attr("transform","translate("+[gistSize/2*index, parseFloat(d) > 0  ? y(0) -rectHe : y(0)   ]+")") ;
         bar1.append("rect")
             .attr("width" , gistSize )
             .attr("height" ,Math.abs( y(0)-y(d) ))
             .attr("fill", color)
-            .on("mousemove", function() { 
-                            div.transition().duration(200).style("opacity", .9);
-                            div.html(   "  " + currencySwapNoCut(d) )
-                                .style("left", (d3.event.pageX) + "px")
-                                .style("top", (d3.event.pageY - 30 ) + "px")
-                                ;
-                            })
-                            .on("mouseout", function() { div.transition().duration(500).style("opacity", 0); })
+            .on("mousemove", function() {
+                div.transition().duration(200).style("opacity", .9);
+                div.html(   "  " + currencySwapNoCut(d) )
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 30 ) + "px")
+                ;
+            })
+            .on("mouseout", function() { div.transition().duration(500).style("opacity", 0); })
         ;
         // bar1.append("text")
         //         .attr("transform","translate("+[gistSize/2, -5]+")")
         //         .attr("class","planGistLabel")
         //         .text(currencySwap(d) )
         // ;
-        
-
-}
 
 
+    }
 
-function showFactIntext(canvas){
-    rects = canvas.append("g").attr("transform","translate("+[50,0]+")")
-    data.forEach(function(d , i){
-        bar = rects.append("g").attr("transform","translate("+[x(i),0]+")")
-                                .attr("width",x(1)-x(0))
-                                .attr("height",getElemHeight(canvas))
-        ;
-        showBartext(bar, 0 ,d.dohodPlan , prop.colorDoh );
-        showBartext(bar, 1 ,d.rashodPlan , prop.colorRash);
-        showBartext(bar, 2 ,d.deficitPlan , prop.colorDef);
 
-    });
-}
 
-function showBartext(canvas, index ,d ,color ){
-        rectHe =  y(0)-y(d)   ;  
+    function showFactIntext(canvas){
+        rects = canvas.append("g").attr("transform","translate("+[50,0]+")")
+        data.forEach(function(d , i){
+            bar = rects.append("g").attr("transform","translate("+[x(i),0]+")")
+                .attr("width",x(1)-x(0))
+                .attr("height",getElemHeight(canvas))
+            ;
+            showBartext(bar, 0 ,d.dohodPlan , prop.colorDoh );
+            showBartext(bar, 1 ,d.rashodPlan , prop.colorRash);
+            showBartext(bar, 2 ,d.deficitPlan , prop.colorDef);
+
+        });
+    }
+
+    function showBartext(canvas, index ,d ,color ){
+        var koef = 1.5;
+        if (index === 0) {
+            koef = 0.55;
+        }
+        rectHe =  y(0)-y(d)   ;
         bar1 = canvas.append("g").attr("transform","translate("+[gistSize/2*index, parseFloat(d) > 0  ? y(0) -rectHe : y(0)   ]+")") ;
         bar1.append("text")
-                .attr("transform","translate("+[gistSize/2, -5]+")")
-                .attr("class","planGistLabel")
-                .text(currencySwap(d) )
+            .attr("transform","translate("+[(gistSize/2) * koef, - 5]+")")
+            .attr("class","planGistLabel")
+            .text(currencySwap(d) )
         ;
 
-}
+    }
 
-function showLegend(canvas){
-    te = canvas.append("g").attr("transform","translate("+[0, 0]+")") ; 
+    function showLegend(canvas){
+        te = canvas.append("g").attr("transform","translate("+[0, 0]+")") ;
 
-    inLeg = te.append("g").attr("transform" , "translate(20,10)");
-    inLeg.append("rect")
-                .attr("width" , 20)
-                .attr("height" , 20)
-                .attr("fill" , prop.colorDoh)
-    ;
-    inLeg.append("text")
-                .attr("transform" , "translate(30 , 15)")
-                .attr("class" , "legend")
-                .text("Доходы")
-    ;
+        inLeg = te.append("g").attr("transform" , "translate(20,10)");
+        inLeg.append("rect")
+            .attr("width" , 20)
+            .attr("height" , 20)
+            .attr("fill" , prop.colorDoh)
+        ;
+        inLeg.append("text")
+            .attr("transform" , "translate(30 , 15)")
+            .attr("class" , "legend")
+            .text("Доходы")
+        ;
 
-    inLeg = te.append("g").attr("transform" , "translate(150,10)");
-    inLeg.append("rect")
-                .attr("width" , 20)
-                .attr("height" , 20)
-                .attr("fill" , prop.colorRash)
-    ;
-    inLeg.append("text")
-                .attr("transform" , "translate(30 , 15)")
-                .attr("class" , "legend")
-                .text("Расходы")
-    ;
+        inLeg = te.append("g").attr("transform" , "translate(150,10)");
+        inLeg.append("rect")
+            .attr("width" , 20)
+            .attr("height" , 20)
+            .attr("fill" , prop.colorRash)
+        ;
+        inLeg.append("text")
+            .attr("transform" , "translate(30 , 15)")
+            .attr("class" , "legend")
+            .text("Расходы")
+        ;
 
-    inLeg = te.append("g").attr("transform" , "translate(270,10)");
-    inLeg.append("rect")
-                .attr("width" , 20)
-                .attr("height" , 20)
-                .attr("fill" , prop.colorDef)
-    ;
-    inLeg.append("text")
-                .attr("transform" , "translate(30 , 15)")
-                .attr("class" , "legend")
-                .text("Дефицит/профцит")
-    ;
+        inLeg = te.append("g").attr("transform" , "translate(270,10)");
+        inLeg.append("rect")
+            .attr("width" , 20)
+            .attr("height" , 20)
+            .attr("fill" , prop.colorDef)
+        ;
+        inLeg.append("text")
+            .attr("transform" , "translate(30 , 15)")
+            .attr("class" , "legend")
+            .text("Дефицит/профцит")
+        ;
 
-}
+    }
 
-function findMaxVal(d){
-    max = d[0].factIn ; 
-    for (i in d)
-        if (max < d[i].factIn)  max = d[i].factIn ; 
-    for (i in d)
-        if (max < d[i].planIn)  max = d[i].planIn ; 
-    return max; 
+    function findMaxVal(d){
+        max = d[0].factIn ;
+        for (i in d)
+            if (max < d[i].factIn)  max = d[i].factIn ;
+        for (i in d)
+            if (max < d[i].planIn)  max = d[i].planIn ;
+        return max;
 
-}
+    }
 
-function findMinVal(d){
-    max = d[0].factOut ; 
-    for (i in d)
-        if (max < d[i].factOut)  max = d[i].factOut ;   
-    for (i in d)
-        if (max < d[i].planOut)  max = d[i].planOut ;   
-    return -max; 
+    function findMinVal(d){
+        max = d[0].factOut ;
+        for (i in d)
+            if (max < d[i].factOut)  max = d[i].factOut ;
+        for (i in d)
+            if (max < d[i].planOut)  max = d[i].planOut ;
+        return -max;
 
-}
-
-
-function findMinVal2(d){
-    mm1 = d[0].dohodPlan ; 
-    for (i in d)
-        for (key in d[i])
-            if (key!="name" && mm1 >  d[i][key]) mm1 =d[i][key] ; 
-    return mm1 ; 
-
-}
-function findMaxVal2(d){
-    ma1 = d[0].dohodPlan ; 
-    for (i in d)
-        for (key in d[i])
-            if (key!="name" && ma1 <  d[i][key]) ma1 =d[i][key] ; 
-    return ma1 ; 
-
-}
+    }
 
 
-function getElemWidth (el){
-    return d3.select(el)._groups["0"]["0"]._groups["0"]["0"].getAttribute("width");
-}
-function getElemHeight (el){
-    return d3.select(el)._groups["0"]["0"]._groups["0"]["0"].getAttribute("height");
-}
+    function findMinVal2(d){
+        mm1 = d[0].dohodPlan ;
+        for (i in d)
+            for (key in d[i])
+                if (key!="name" && mm1 >  d[i][key]) mm1 =d[i][key] ;
+        return mm1 ;
+
+    }
+    function findMaxVal2(d){
+        ma1 = d[0].dohodPlan ;
+        for (i in d)
+            for (key in d[i])
+                if (key!="name" && ma1 <  d[i][key]) ma1 =d[i][key] ;
+        return ma1 ;
+
+    }
+
+
+    function getElemWidth (el){
+        return d3.select(el)._groups["0"]["0"]._groups["0"]["0"].getAttribute("width");
+    }
+    function getElemHeight (el){
+        return d3.select(el)._groups["0"]["0"]._groups["0"]["0"].getAttribute("height");
+    }
 
 
 
-function cutLongSum(d){
-    if ( Math.abs(d) > 1000)
-        if (+d > 0 )
-            return  d.toString().substr(0, -max.toString().length +d.toString().length  +3) ; 
+    function cutLongSum(d){
+        d =Math.abs(d) ;
+        if ( d > 100)
+            if (+d > 0 )
+                return  d.toString().substr(0, d.toString().length-max.toString().length   +3) ;
+            else
+                return  ( d.toString().substr(0, d.toString().length-min.toString().length   +4  ) ) ;
         else
-            return  ( d.toString().substr(0, -min.toString().length +d.toString().length  +4  ) ) ; 
-    else 
-        return d ; 
-}
+            return d ;
+    }
+
     function showBorders(canvas ) {
         g = canvas.append("g");
         line = d3.line().x(function(d) {return d[0];} ).y(function(d) { return d[1]} ) ;
@@ -350,7 +382,7 @@ function cutLongSum(d){
 
 
     function currencySwap(d) {
-        d = parseInt(d * 0.001);
+        d = parseInt(d * de);
         return d.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") + "";
     }
 
@@ -364,27 +396,38 @@ function cutLongSum(d){
             return str ;
     }
 
-    function breakLongText (str , limit) { 
-
+    function breakLongText (str , limit) {
+        limBack =limit ;
         if (str.length > limit){
-            s="";
-            if (str[limit-1]!=" ") for (i=limit;i< limit+10;i++) {
-                if (str[i]==" ") {
-                    limit=i+1;
-                    break;
+            s=""; count=0  ; limitStart=0;
+            while (limit < str.length && count < 10 ){
+                if (str[limit]!=" ") {
+                    for ( i = limitStart; i < str.length  ; i++) {
+                        limit = i + 1;
+                        if (str[i] == " " && (limit - limitStart) > limBack )  break;
+                    }
+                } else {
+                    for ( i = limitStart; i < str.length  ; i++) {
+                        limit = i + 1;
+                        if (str[i] == " " && (limit - limitStart) > limBack )  break;
+                    }
                 }
+
+                s1= str.substr(limitStart, limit - limitStart) ;
+                // console.log(count , limitStart , limit ,s1);
+                s += "<tspan y='" + (-20 + 20 * count ) + "' x='0' dy='1.2em'>" +s1+ "</tspan>";
+                limitStart = limit ;
+
+                count++ ;
             }
-            s = "<tspan y='-20' x='0' dy='1.2em'>" + str.substr(0,limit) + "</tspan>" ;
-            s += "<tspan x='0' dy='1.2em'>" + str.substr(limit) + "</tspan>" ;
             return s ;
-        }
-        else
-            return str ;
+
+        } else  return str ;
     }
 
 
     function currencySwapWithText(d) {
-        d= parseInt(parseFloat(d)*0.001 );
+        d= parseInt(parseFloat(d) );
         return d.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " тыс. руб.";
     }
 
@@ -394,12 +437,7 @@ function cutLongSum(d){
         return d.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " руб.";
     }
 
-    function cutLongSum(d) {
-        if (+d > 1000)
-            return d.toString().substr(0, 3);
-        else
-            return d;
-    }
+
 
     function getElemWidth(el) {
         return d3.select(el)._groups["0"]["0"]._groups["0"]["0"].getAttribute("width");
@@ -411,13 +449,12 @@ function cutLongSum(d){
 
 
     drawAsisY(asisY);
- 
-gistSize = barSize / 3 ; 
-gistSize = 50 ; 
 
-showLegend(asisX) ; 
-showFactIn(gist);
-showFactIntext(gist);
+
+
+    showLegend(asisX) ;
+    showFactIn(gist);
+    showFactIntext(gist);
 
     drawAsisX(gist);
 }
--- graf40/30/prop.js
+++ graf40/30/prop.js
@@ -19,3 +19,5 @@ var dataGraph30 = [
 ]
 
 
+
+var dataGraph30 =  [{"name":"Республика Калмыкия","dohodPlan":11637065128.4,"rashodPlan":12196410336.87,"deficitPlan":-559345208.47},{"name":"Краснодарский край","dohodPlan":263308449944.56,"rashodPlan":260865826520.27,"deficitPlan":2442623424.29},{"name":"Астраханская область","dohodPlan":38863548575.07,"rashodPlan":40457752381.5,"deficitPlan":-1594203806.43},{"name":"Волгоградская область","dohodPlan":101485789274.43,"rashodPlan":108067233540.48,"deficitPlan":-6581444266.05},{"name":"Ростовская область","dohodPlan":190895127447.6,"rashodPlan":186359236478.14,"deficitPlan":4535890969.46},{"name":"г.Севастополь","dohodPlan":21818398672.1,"rashodPlan":25334208974.87,"deficitPlan":-3515810302.77},{"name":"Республика Крым","dohodPlan":116745620833.55,"rashodPlan":131317800090.65,"deficitPlan":-14572179257.1},{"name":"Республика Адыгея (Адыгея)","dohodPlan":18113526690.39,"rashodPlan":17675402046.61,"deficitPlan":438124643.78}]
